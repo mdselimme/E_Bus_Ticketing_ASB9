bae335d43796cc4282ed4f8bc462de3e745fdcdc
mdselimme <mdselimdev@gmail.com>
2024-10-03 00:12:44 +0600

    add_js_code

--- js/seat_select.js
+++ js/seat_select.js
@@ -1,8 +1,25 @@
 
 const allSeats = document.getElementById("seat-box-main");
 
+const children = allSeats.children;
+
+const arr = [];
+
 allSeats.addEventListener("click", function(e){
-    e.target.classList.add("seat-click");
-    
-})
+   
+    const mainEv = e.target;
+   if(!arr.includes(mainEv.innerText) && arr.length !== 4){
+    arr.push(mainEv.innerText);
+    mainEv.classList.add("seat-click")
+   }else{
+        mainEv.classList.remove("seat-click");
+        const ind = arr.indexOf(mainEv.innerText);
+        if (ind > -1) { 
+            arr.splice(ind, 1); 
+        } 
+   };
+   
+   
+    e.stopImmediatePropagation();
+});
 

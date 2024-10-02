a576e458410c1a083fd2f561a19c932344dd1988
mdselimme <mdselimdev@gmail.com>
2024-10-03 00:32:45 +0600

    add_js_code

--- js/seat_select.js
+++ js/seat_select.js
@@ -1,4 +1,5 @@
 
+// All seats find 
 const allSeats = document.getElementById("seat-box-main");
 
 const children = allSeats.children;
@@ -6,11 +7,10 @@ const children = allSeats.children;
 const arr = [];
 
 allSeats.addEventListener("click", function(e){
-   
     const mainEv = e.target;
-   if(!arr.includes(mainEv.innerText) && arr.length !== 4){
-    arr.push(mainEv.innerText);
-    mainEv.classList.add("seat-click")
+    if(!arr.includes(mainEv.innerText) && arr.length !== 4){
+        arr.push(mainEv.innerText);
+        mainEv.classList.add("seat-click")
    }else{
         mainEv.classList.remove("seat-click");
         const ind = arr.indexOf(mainEv.innerText);
@@ -18,8 +18,6 @@ allSeats.addEventListener("click", function(e){
             arr.splice(ind, 1); 
         } 
    };
-   
-   
     e.stopImmediatePropagation();
 });
 
